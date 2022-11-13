303b338153faffa5bbdb776534cc73495ff5b6ca
Marko <marko.bolt4@gmail.com>
2022-11-13 16:41:00 +0100

    added

--- Services/Subscriptions/ISubscriptionService.cs
+++ Services/Subscriptions/ISubscriptionService.cs
@@ -1,14 +1,11 @@
 ﻿using AutoMapper;
 using Span.Culturio.Api.Data;
+using Span.Culturio.Api.Models.Subscriptions;
 
-namespace Span.Culturio.Api.Services.Subscriptions {
-    public class ISubscriptionService {
-        private readonly DataContext _context;
-        private readonly IMapper _mapper;
-
-        public ISubscriptionService(DataContext context, IMapper mapper) {
-            _context = context;
-            _mapper = mapper;
-        }
+namespace Span.Culturio.Api.Services.Subscriptions
+{
+    public interface ISubscriptionService {
+        Task<SubscriptionDto> CreateAsync(CreateSubscriptionDto createSubscriptionDto);
+        Task<SubscriptionDto> GetAsync(int userId);
     }
 }
---
+++ Services/Subscriptions/SubscriptionService.cs
@@ -0,0 +1,13 @@
+﻿using Span.Culturio.Api.Models.Subscriptions;
+
+namespace Span.Culturio.Api.Services.Subscriptions {
+    public class SubscriptionService : ISubscriptionService {
+        public Task<SubscriptionDto> CreateAsync(CreateSubscriptionDto createSubscriptionDto) {
+            throw new NotImplementedException();
+        }
+
+        public Task<SubscriptionDto> GetAsync(int userId) {
+            throw new NotImplementedException();
+        }
+    }
+}
